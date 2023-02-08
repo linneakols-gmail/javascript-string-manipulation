ec5501eba3fb5536583d3d11bf51db512e23a88b
Linnea Kols <linnea.kols@knowit.se>
2023-02-08 16:25:46 +0100

    Updated again

--- src/info.js
+++ src/info.js
@@ -8,13 +8,13 @@ Converts text between newline (\n), carriage return (\r), and combined
 Options:
 
 [CR|LF|CRLF]: Desired line ending
-              
+
     CR is a carriage return character \r (rare)
     LF is a line feed character \n (common on Unix and macOS)
     CRLF is a combined ending \r\n (common on Windows)
-          
+
 Example:
-             
+
     cat in.txt | crlf-convert LF > out.txt
 `;
 
